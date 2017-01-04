64993e096d89f31c60d8ce56fca677048aeb251d
ObstBoxGit <obst.box@gmail.com>
2017-01-04 21:30:37 +0500

    admin panel1

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,12 @@
 <?php
-    echo "Admin panel";
+    
+    //echo "Admin panel";
+	require_once("../database.php");
+    require_once("../models/articles.php");
+
+	$link = db_connect();
+	$articles = articles_all($link);
+	
+	include("../views/articles_admin.php");
 ?>
 
