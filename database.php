ada29cc45de077a5e694975417535813df3201f4
ObstBoxGit <obst.box@gmail.com>
2017-01-04 18:26:48 +0500

    bug fixes

--- database.php
+++ database.php
@@ -5,11 +5,21 @@ define('MYSQL_PASSWORD','root');
 define('MYSQL_DB','blog');
 function db_connect(){
 	$link = mysqli_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB)
-				or die("Error: " . mysql_error($link));
+				or die("Error: " . mysqli_errno($link));
+	
+	/*
+	$link = mysqli_connect('localhost', 'root', 'root', 'blog');
+	if (mysqli_errno($link)) {
+		die("failed to connect to mysql: " . mysqli_connect_error() );
+	}
+	*/
 	
 	if(!mysqli_set_charset($link, 'utf8')){
 		printf("Error: " . mysql_error($link));
 	}
+	
+	// это должно быть не здесь!
+	// ыmysqli_close($link);
 	
 	return $link;
 }
--- index.php
+++ index.php
@@ -2,8 +2,13 @@
     require_once("database.php");
     require_once("models/articles.php");
 
+	
 	$link = db_connect();
-    $articles = articles_all();
+    $articles = articles_all($link);
+    
+    //~ echo "<pre>";
+    //~ var_dump($article);
+	//~ echo "</pre>";
     
     include("views/article.php");
 ?>
